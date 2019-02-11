120b8670b4e3dad909fc097ca396d517bf3618fe
xmzhang.abcft <xmzhang@abcft.com>
2019-02-11 18:53:01 +0800

    准备基础配置

--- manage.py
+++ manage.py
@@ -8,13 +8,12 @@ from redis import StrictRedis
 # 链接数据库之前需要先链接数据库  获取数据库控制对象
 class Config(object):
     """配置信息"""
-    # 开启DEBUG模式
-    DEBUG = True
+    DEBUG = True  # 开启DEBUG模式
 
     # 设置相应的mysql连接方式
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/flask"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:mysql@127.0.0.1:3306/flask"  # 连接数据库,使用pymysql需要+pymysql
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # 动态追踪修改设置，如未设置只会提示警告
+    SQLALCHEMY_ECHO = True  # 显示转换后的原生sql语句
 
     # 配置redis相关参数
     REDIS_HOST = "127.0.0.1"  # redis链接
